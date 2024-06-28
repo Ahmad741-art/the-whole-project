f881a50c4af97398c75aabd69cc1c9645c878cc7
Ahmad741-art <155184460+Ahmad741-art@users.noreply.github.com>
2024-06-28 22:50:41 +0200

    Add files via upload

---
+++ models.py
@@ -0,0 +1,15 @@
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+    transactions = db.relationship('Transaction', backref='user', lazy=True)
+
+class Transaction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    type = db.Column(db.String(50), nullable=False)  # Income or Expense
+    amount = db.Column(db.Float, nullable=False)
+    category = db.Column(db.String(100), nullable=False)
+    date = db.Column(db.Date, nullable=False)
